de9961daad511b76f43d99078ef8d62a2b11f592
LinkHpp <linkhpp4pokemon@gmail.com>
2024-02-13 13:39:22 +0100

    API now works thanks to properties

--- src/main/java/com/example/demo/repository/ICourseRepository.java
+++ src/main/java/com/example/demo/repository/ICourseRepository.java
@@ -2,8 +2,6 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource(path = "courses", collectionResourceRel = "courses")
 public interface ICourseRepository extends JpaRepository<Course, Integer> {
 }
